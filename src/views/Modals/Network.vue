677e5f7486915f37e1c770c4625f1b658013de0c
Esteban Dimier <esteban.dimier@mahjopi.com>
2022-04-22 11:40:18 +0200

    [EDITOR] Network form finished

--- src/views/Modals/Network.vue
+++ src/views/Modals/Network.vue
@@ -16,7 +16,7 @@
         "
       >
         <div class="max-w-2xl p-6 bg-white rounded-md shadow-xl">
-          <div class="flex items-center justify-between">
+          <div class="flex items-center justify-between mb-4">
             <h3 class="text-2xl">Réseau VPC</h3>
             <svg
               @click="isOpen = false"
@@ -34,21 +34,45 @@
               />
             </svg>
           </div>
-          <div class="mt-4">
+          <div class="flex justify-center">
+              <div class="form-check float-left mr-10">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="netType" id="netType1">
+                <label class="form-check-label inline-block text-black" for="netType1">
+                  Nouveau
+                </label>
+              </div>
+              <div class="form-check float-left">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="netType" id="netType2" checked>
+                <label class="form-check-label inline-block text-black" for="netType2">
+                  Existant
+                </label>
+              </div>
+          </div>
+          <div class="mt-3">
             <div class="flex justify-center">
-              <div>
-                <div class="form-check float-left mr-10">
-                  <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                  <label class="form-check-label inline-block text-black" for="flexRadioDefault1">
-                    Nouveau
-                  </label>
+              <div class="mb-3 xl:w-96">
+                <div class="text-right">
+                  <label for="region" class="form-label inline-block mb-0.5 text-black">Région</label>
                 </div>
-                <div class="form-check float-left">
-                  <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                  <label class="form-check-label inline-block text-black" for="flexRadioDefault2">
-                    Existant
-                  </label>
+                <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
+                    <option selected></option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="mb-3 xl:w-96">
+                <div class="text-right">
+                  <label for="region" class="form-label inline-block mb-0.5 text-black">Réseau</label>
                 </div>
+                <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
+                    <option selected></option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
               </div>
             </div>
             <div class="flex justify-center">
@@ -70,7 +94,7 @@
             <div class="flex justify-center">
               <div class="mb-3 xl:w-96">
                 <div class="text-right">
-                  <label for="desc" class="form-label inline-block mb-0.5 text-black">Région</label>
+                  <label for="region" class="form-label inline-block mb-0.5 text-black">Région</label>
                 </div>
                 <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
                     <option selected></option>
@@ -81,11 +105,86 @@
               </div>
             </div>
             <div class="flex justify-center">
-              <div class="mb-3 xl:w-96">
+              <div class="mb-4 xl:w-96">
                 <div class="text-right">
-                  <label for="desc" class="form-label inline-block mb-0.5 text-black">Adresse réseau</label>
+                  <label for="netAddr" class="form-label inline-block mb-0.5 text-black">Adresse réseau</label>
                 </div>
-                <input type="text" class="form-control block w-full px-3 py-1.5 text-right text-base font-normal text-black bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none" id="addr" placeholder="Exemple : 10.128.0.0/24" />
+                <input type="text" class="form-control block w-full px-3 py-1.5 text-right text-base font-normal text-black bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none" id="netAddr" placeholder="Exemple : 10.128.0.0/24" />
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="text-center mb-3">
+                <label for="region" class="form-label inline-block text-xl mb-0.5 text-black">Routage dynamique</label>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="form-check float-left mr-10">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="routing" id="routing1">
+                <label class="form-check-label inline-block text-black" for="netType1">
+                  Régional
+                </label>
+              </div>
+              <div class="form-check float-left">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="routing" id="routing2" checked>
+                <label class="form-check-label inline-block text-black" for="netType2">
+                  Mondial
+                </label>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="text-center mb-3 mt-4">
+                <label for="region" class="form-label inline-block text-xl mb-0.5 text-black">MTU</label>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="form-check float-left mr-10">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="routing" id="routing1">
+                <label class="form-check-label inline-block text-black" for="netType1">
+                  1500
+                </label>
+              </div>
+              <div class="form-check float-left">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="routing" id="routing2" checked>
+                <label class="form-check-label inline-block text-black" for="netType2">
+                  1460
+                </label>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="text-center mb-3 mt-4">
+                <label for="region" class="form-label inline-block text-xl mb-0.5 text-black">Pare-feu</label>
+              </div>
+            </div>
+            <div class="flex justify-center mb-4">
+              <div>
+                <div class="form-check float-left mr-10">
+                  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form-check-label inline-block text-gray-800" for="flexCheckDefault">
+                    SSH
+                  </label>
+                </div>
+                <div class="form-check float-left">
+                  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckChecked" checked>
+                  <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">
+                    RDP
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="w-auto float-left">
+                <div class="form-check float-left mr-10">
+                  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form-check-label inline-block text-gray-800" for="flexCheckDefault">
+                    ICMP
+                  </label>
+                </div>
+                <div class="form-check float-left">
+                  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckChecked" checked>
+                  <label class="form-check-label inline-block underline text-gray-800" for="flexCheckChecked">
+                    Personnalisé
+                  </label>
+                </div>
               </div>
             </div>
           </div>
