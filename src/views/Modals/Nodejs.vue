0499db220131e8f842315bad3fef1f5a1580798a
Esteban Dimier <esteban.dimier@mahjopi.com>
2022-04-22 16:30:14 +0200

    [EDITOR] NodeJS root placeholder

--- src/views/Modals/Nodejs.vue
+++ src/views/Modals/Nodejs.vue
@@ -40,7 +40,7 @@
                 <div class="text-right">
                   <label for="name" class="form-label inline-block mb-0.5 text-black">Racine du serveur (cible)</label>
                 </div>
-                <input type="text" class="form-control block w-full px-3 py-1.5 text-right text-base font-normal text-black bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none" id="name" />
+                <input type="text" class="form-control block w-full px-3 py-1.5 text-right text-base font-normal text-black bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none" id="name" placeholder="/repo/exemple"/>
               </div>
             </div>
             <div class="flex justify-center">
