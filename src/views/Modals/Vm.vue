117f318aff88a6a474f9d59211da0662c2ed638a
Esteban Dimier <esteban.dimier@mahjopi.com>
2022-04-26 10:35:06 +0200

    [EDITOR] VM form v2

--- src/views/Modals/Vm.vue
+++ src/views/Modals/Vm.vue
@@ -162,7 +162,7 @@
                 </div>
                 <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
                     <option selected></option>
-                    <option value="1">One</option>
+                    <option value="1">SSD</option>
                     <option value="2">Two</option>
                     <option value="3">Three</option>
                 </select>
@@ -181,7 +181,7 @@
                 </div>
                 <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
                     <option selected></option>
-                    <option value="1">One</option>
+                    <option value="1">HDD</option>
                     <option value="2">Two</option>
                     <option value="3">Three</option>
                 </select>
@@ -263,6 +263,96 @@
                     HTTPS
                   </label>
                 </div>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="text-center mb-3 mt-4">
+                <label for="region" class="form-label inline-block text-xl mb-0.5 text-black">Réseau</label>
+              </div>
+            </div>
+            <div class="flex justify-center mb-4">
+              <div class="form-check float-left mr-10">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="routing" id="routing1">
+                <label class="form-check-label inline-block text-black" for="netType1">
+                  Par défaut
+                </label>
+              </div>
+              <div class="form-check float-left">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="routing" id="routing2" checked>
+                <label class="form-check-label inline-block text-black" for="netType2">
+                  Personnalisé
+                </label>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="mb-3 xl:w-96">
+                <div class="text-right">
+                  <label for="name" class="form-label inline-block mb-0.5 text-black">Réseau</label>
+                </div>
+                <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
+                    <option selected></option>
+                    <option value="1">pimahtic-network</option>
+                    <option value="2">net</option>
+                    <option value="3">work</option>
+                </select>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="mb-3 xl:w-96">
+                <div class="text-right">
+                  <label for="name" class="form-label inline-block mb-0.5 text-black">Sous-réseau</label>
+                </div>
+                <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
+                    <option selected></option>
+                    <option value="1">pimahtic-network-one</option>
+                    <option value="2">net</option>
+                    <option value="3">work</option>
+                </select>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="mb-3 xl:w-96">
+                <div class="text-right">
+                  <label for="name" class="form-label inline-block mb-0.5 text-black">Adresse IP interne principale</label>
+                </div>
+                <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
+                    <option selected></option>
+                    <option value="1">Éphèmère (automatique)</option>
+                    <option value="2">net</option>
+                    <option value="3">work</option>
+                </select>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="mb-3 xl:w-96">
+                <div class="text-right">
+                  <label for="name" class="form-label inline-block mb-0.5 text-black">Plage d'adresses IP d'alias</label>
+                </div>
+                <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-black bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-purple-600 focus:outline-none">
+                    <option selected></option>
+                    <option value="1">Éphèmère</option>
+                    <option value="2">net</option>
+                    <option value="3">work</option>
+                </select>
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <div class="text-center mt-4 mb-1">
+                <label for="region" class="form-label inline-block mb-0.5 text-black">Niveau de service réseau</label>
+              </div>
+            </div>
+            <div class="flex justify-center mb-4">
+              <div class="form-check float-left mr-10">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="routing" id="routing1">
+                <label class="form-check-label inline-block text-black" for="netType1">
+                  Premium
+                </label>
+              </div>
+              <div class="form-check float-left">
+                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="routing" id="routing2" checked>
+                <label class="form-check-label inline-block text-black" for="netType2">
+                  Standard
+                </label>
               </div>
             </div>
           </div>
