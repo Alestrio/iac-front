b70855cab5e5dfcd51eaef4bd622365f47541706
Alexis LEBEL <alexis.lebel@mahjopi.com>
2022-05-03 16:16:32 +0200

    [EDITOR/GOOGLENET] Fixed radio

--- src/views/Modals/GoogleNetwork.vue
+++ src/views/Modals/GoogleNetwork.vue
@@ -315,35 +315,33 @@
             </div>
             <div class="flex justify-center">
               <div class="form-check float-left mr-10">
-                <input
-                  class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="radio"
-                  name="routing"
-                  id="routing1"
-                  value="REGIONAL"
-                  v-model="this.gcp_network.routing_type"
-                />
                 <label
                   class="form-check-label inline-block text-black"
-                  for="netType1"
                 >
+                  <input
+                    class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                    type="radio"
+                    name="routing"
+                    id="routing1"
+                    value="REGIONAL"
+                    v-model="this.gcp_network.routing_type"
+                  />
                   Régional
                 </label>
               </div>
               <div class="form-check float-left">
-                <input
-                  class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="radio"
-                  name="routing"
-                  id="routing2"
-                  checked
-                  value="GLOBAL"
-                  v-model="this.gcp_network.routing_type"
-                />
                 <label
                   class="form-check-label inline-block text-black"
-                  for="netType2"
                 >
+                  <input
+                    class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-purple-600 checked:border-purple-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                    type="radio"
+                    name="routing"
+                    id="routing2"
+                    checked
+                    value="GLOBAL"
+                    v-model="this.gcp_network.routing_type"
+                  />
                   Mondial
                 </label>
               </div>
