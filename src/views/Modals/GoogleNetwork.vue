626e91fc43c178979c438baa5382bbc3cc1ea469
Alexis LEBEL <alexis.lebel@mahjopi.com>
2022-05-04 10:49:11 +0200

    [EDITOR/GOOGLENET] Corrected select drawer

--- src/views/Modals/GoogleNetwork.vue
+++ src/views/Modals/GoogleNetwork.vue
@@ -257,9 +257,9 @@
                   >
                 </div>
                 <select
-                  @change="updateGCPZone($event.target.value)"
                   class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   aria-label="GCP Zone"
+                  v-model="this.sample_subnet.gcp_zone"
                 >
                   <option selected>{{ this.selected_gcp_zone }}</option>
                   <option
