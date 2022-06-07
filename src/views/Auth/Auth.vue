0fc2c8688e6e98f17ee665ed76befd14bbeb834c
Alexis LEBEL <alexis.lebel@mahjopi.com>
2022-06-07 16:19:34 +0200

    [LOGIN] Enhanced login form

--- src/views/Auth/Auth.vue
+++ src/views/Auth/Auth.vue
@@ -1,35 +1,37 @@
-/* * Copyright (c) 2022 Alexis LEBEL * All rights reserved. */ /* * Copyright
-(c) 2022 Alexis LEBEL * All rights reserved. */
+/* 
+* Copyright (c) 2022 Alexis LEBEL 
+* All rights reserved. 
+*/
 <template>
   <div class="w-full h-screen md:p-4 flex justify-center items-center">
     <main
-      class="w-full md:w-2/6 xl:w-2/6 h-9/10 flex items-center shadow-xl bg-gray-100 rounded-lg font-mono"
+      class="w-full md:w-2/6 xl:w-2/6 h-9/10 flex items-center shadow-xl bg-gray-100 rounded-lg"
     >
       <div class="w-full md:w-3/3 p-5 grid grid-cols-1 space-y-6 text-center">
         <span class="font-bold text-2xl text-purple-500">Projet IaC</span>
         <Form
-          class="space-y-3"
+          class="space-y-3 flex flex-col w-1/2 m-auto"
           @submit="handleLogin"
           :validation-schema="schema"
         >
           <Field
-            class="w-1/2 p-1 m-1 rounded-md text-purple-500 text-center focus:border-purple-600 focus:outline-none"
+            class="w-full p-1 m-1 rounded-md text-purple-500 text-center focus:border-purple-600 focus:outline-none"
             type="text"
             placeholder="Nom d'utilisateur"
             v-model="username"
             name="username"
           />
-          <ErrorMessage name="username" class="alert-red-500" />
+          <ErrorMessage name="username" class="text-red-500 bg-red-100 rounded p-2" />
           <Field
-            class="w-1/2 p-1 m-1 rounded-md text-purple-500 text-center focus:border-purple-600 focus:outline-none"
+            class="w-full p-1 m-1 rounded-md text-purple-500 text-center focus:border-purple-600 focus:outline-none"
             type="password"
             placeholder="Mot de passe"
             v-model="password"
             name="password"
           />
-          <ErrorMessage name="password" class="alert-red-500" />
+          <ErrorMessage name="password" class="text-red-500 bg-red-100 rounded p-2" />
           <button
-            class="w-1/2 p-1 m-1 rounded-md text-white text-center focus:border-purple-600 focus:outline-none bg-purple-500 hover:bg-purple-600"
+            class="w-full p-1 m-1 rounded-md text-white text-center focus:border-purple-600 focus:outline-none bg-purple-500 hover:bg-purple-600"
             :disabled="loading"
           >
             <div
