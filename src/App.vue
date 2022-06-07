fbdc3cdc7fa6b6ff77ec4fecbfaa0285c746bf3a
Alexis LEBEL <alexis.lebel@mahjopi.com>
2022-06-07 15:45:43 +0200

    [AUTH] Added Logout

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,28 @@
               Logs
             </router-link>
           </div>
+          <div class="flex flex-row">
+            <a
+              class="text-xl hover:bg-gray-200 rounded p-1 w-full"
+              @click.prevent="logOut"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+              Logout
+            </a>
+          </div>
           <div>
             <!-- zone selection select -->
             <div class="flex flex-col justify-center">
